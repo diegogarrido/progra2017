22767e5fb000a8fb66527c45ab767dc5327e8818
diegogarrido <chicodie04@gmail.com>
2017-04-01 18:49:55 -0300

    Métodos estáticos, uso instanceof

---
+++ mitocode/instancias/Galleta.java
@@ -0,0 +1,11 @@
+package pkginstanceof;
+
+
+public class Galleta {
+    String nombre;
+
+    public Galleta(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
---
+++ mitocode/instancias/Manzana.java
@@ -0,0 +1,18 @@
+package pkginstanceof;
+
+
+public class Manzana extends Fruta{
+
+    public Manzana(String nombre) {
+        super(nombre);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
